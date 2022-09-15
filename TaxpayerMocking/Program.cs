be5b29044852d413db74f21588f0b0343208c81a
Karen Payne <paynekaren@comcast.net>
2022-09-15 04:56:02 -0700

    updates

--- TaxpayerMocking/Program.cs
+++ TaxpayerMocking/Program.cs
@@ -1,4 +1,8 @@
-﻿using TaxpayerMocking.Classes;
+﻿
+using Newtonsoft.Json;
+using TaxpayerLibraryEntityVersion.Models;
+using TaxpayerMocking.Classes;
+using TaxpayerMocking.Models;
 
 namespace TaxpayerMocking
 {
@@ -7,21 +11,48 @@ namespace TaxpayerMocking
         static void Main(string[] args)
         {
 
-            SetupDatabase.Initialize(5);
-            var taxpayers = SetupDatabase.GetTaxpayers();
+            SetupDatabase.Initialize(2);
+            List<Taxpayer> taxpayerList = SetupDatabase.GetTaxpayers();
+
+            //JsonExample(taxpayerList);
 
             var table = CreateTable();
 
             AnsiConsole.Clear();
 
-            foreach (var taxpayer in taxpayers)
+            foreach (var taxpayer in taxpayerList)
             {
-                table.AddRow(taxpayer.Id.ToString(), taxpayer.FullName, taxpayer.SocialSecurityNumber, taxpayer.Pin,
-                    taxpayer.StartDate.Value.ToString("MM/dd/yyyy"), taxpayer.Category.Description);
+                if (taxpayer.StartDate.HasValue)
+                {
+                    table.AddRow(taxpayer.Id.ToString(),
+                        taxpayer.FullName,
+                        taxpayer.SocialSecurityNumber,
+                        taxpayer.Pin,
+                        taxpayer.StartDate.Value.ToString("MM/dd/yyyy"),
+                        taxpayer.Category.Description);
+                }
             }
 
             AnsiConsole.Write(table);
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// How to serialize a self-referencing model
+        /// </summary>
+        private static void JsonExample(List<Taxpayer> taxpayerList)
+        {
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects, 
+                Formatting = Formatting.Indented
+            };
+
+            string json = JsonConvert.SerializeObject(taxpayerList, jsonSerializerSettings);
+            File.WriteAllText("Taxpayers.json", json);
+            List<Taxpayer> taxpayers = JsonConvert.DeserializeObject<List<Taxpayer>>(json);
+
+            // put breakpoint here to examine the list
         }
     }
 }
--- TaxpayerMocking/readme.md
+++ TaxpayerMocking/readme.md
@@ -10,3 +10,44 @@ Not something an enterprise developer would do, instead the better method is to 
 > There are no free extensions or tools to reverse engineer Oracle. A developer needs to use the dotnet CLI tools to scaffold an Oracle database or create your own tool such as one Karen created shown below.
 
 ![Oracle](assets/oracle.png)
+
+# Generating database on the fly
+
+This is done by EF Core looking to the DbContext, get the DbSet objects, looks at the models for the DbSet objects then if a model has an int property named Id or ends in Id creates primary and foreign keys which is also dependent on properties that are from another model which can be a single instance or a list.
+
+Given
+
+```csharp
+public partial class Taxpayer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName => $"{FirstName} {LastName}";
+    public string SSN { get; set; }
+    public string SocialSecurityNumber => SSN.Insert(5, "-").Insert(3, "-");
+    public string Pin { get; set; }
+    public DateTime? StartDate { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public override string ToString() => $"{FirstName} {LastName}";
+}
+
+public class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+    public string Description { get; set; }
+    public List<Taxpayer> Taxpayers { get; set; }
+
+    public Category()
+    {
+        Taxpayers = new List<Taxpayer>();
+    }
+}
+```
+
+EF Core generates
+
+![Diagram](assets/diagram.png)
+
