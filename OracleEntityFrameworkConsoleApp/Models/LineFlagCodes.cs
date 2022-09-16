1c8a10f87c9494d8dadce1e1924922cd8d305fe3
Karen Payne <paynekaren@comcast.net>
2022-09-16 10:47:27 -0700

    moved file

--- OracleEntityFrameworkConsoleApp/Models/LineFlagCodes.cs
+++ OracleEntityFrameworkConsoleApp/Models/LineFlagCodes.cs
@@ -14,29 +14,3 @@ internal class LineFlagCodes
     [Column("VALID_FLAG")]
     public string ValidFlag { get; set; }
 }
-[Keyless]
-[Table("FEDERAL_RESERVE_ROUTINGS")]
-internal class FederalReserveRouting
-{
-    [Column("ROUTING_NUM")]
-    public string RoutingNumber { get; set; }
-
-    [Column("BANK_NAME")]
-    public string BankName { get; set; }
-
-    [Column("BANK_ADDRESS_CITY")]
-    public string BankAddressCity { get; set; }
-
-    [Column("BANK_ADDRESS_STATE")]
-    public string BankAddressState { get; set; }
-
-    [Column("LAST_REVISION_DATE")]
-    public DateTime LastRevisionDate { get; set; }
-
-    [Column("POST_DATE")]
-    public DateTime PostDate { get; set; }
-
-    [Column("VALID_FLAG")]
-    public string Flag { get; set; }
-
-}
